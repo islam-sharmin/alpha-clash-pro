4107d083827ba57a0df4f56b557338d6e26d3b96
Sharmin2622 <islam.sharmin111@gmail.com>
2024-02-16 18:34:03 +0600

    work on score and life

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -21,12 +21,53 @@ function handleKeyBoardKeyUpEvent(event) {
 
     // check matched or not
     if (playerPressed === expectedAlphabet) {
-        // console.log('you get a point');
-        console.log('you have pressed correctly', expectedAlphabet);
+        console.log('you get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        // -----------------------
+        // update score:
+        // // 1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+
+        // // 2. increase the score by 1
+        // const newScore = currentScore + 1;
+
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        // console.log('you have pressed correctly', expectedAlphabet);
+
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
+
     } else {
         console.log('you lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+
+
+        // -----------------------
+        // reduce life
+        // // step-1: get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // step-2: reduce the life count 
+        // const nowLife = currentLife - 1;
+        // // step-3: display the updated life count
+
+        // currentLifeElement.innerText = nowLife;
     }
 }
 // capture keyboard key press
