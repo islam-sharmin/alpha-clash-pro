44e842b0a7604acd1c954daf34ede349a19e2f21
Sharmin2622 <islam.sharmin111@gmail.com>
2024-02-16 23:31:51 +0600

    js score board added

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -11,9 +11,14 @@
 
 function handleKeyBoardKeyUpEvent(event) {
     const playerPressed = event.key;
-    // console.log('player pressed ', playerPressed);
+    console.log('player pressed ', playerPressed);
 
-    // get the expected to press
+    // stop the game if pressed 'Esc'
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+
+    // key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
@@ -21,7 +26,7 @@ function handleKeyBoardKeyUpEvent(event) {
 
     // check matched or not
     if (playerPressed === expectedAlphabet) {
-        console.log('you get a point');
+        // console.log('you get a point');
 
         const currentScore = getTextElementValueById('current-score');
         const updatedScore = currentScore + 1;
@@ -48,13 +53,15 @@ function handleKeyBoardKeyUpEvent(event) {
         continueGame();
 
     } else {
-        console.log('you lost a life');
+        // console.log('you lost a life');
 
         const currentLife = getTextElementValueById('current-life');
         const updatedLife = currentLife - 1;
         setTextElementValueById('current-life', updatedLife);
 
-
+        if (updatedLife === 0) {
+            gameOver();
+        }
 
         // -----------------------
         // reduce life
@@ -87,7 +94,31 @@ function continueGame() {
 }
 
 function play() {
+    // hide everything show only the playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score
+    // 1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    // console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+
+    // clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+
 }
--- js/utility.js
+++ js/utility.js
@@ -30,6 +30,12 @@ function setTextElementValueById(elementId, value) {
     element.innerText = value;
 }
 
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function getRandomAlphabet() {
     // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
